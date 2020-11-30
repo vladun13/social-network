b90969392942b6ff201f6de17085e25c138ac6e9
Vlad <nemyrovsky1311vlad@gmail.com>
2020-11-30 20:57:30 +0200

    First cganges

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.designevo.com/images/home/3d-green-letter-o.png" />
+    </header>
+  );
+};
+
+export default Header;
